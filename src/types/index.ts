d0495fee7891d47f3755725949b9534e83afd09e
Henry <chunhyungjoon@gmail.com>
2023-09-08 14:10:04 +1000

    finalise project

--- src/types/index.ts
+++ src/types/index.ts
@@ -43,3 +43,22 @@ export interface FilterProps {
   limit?: number;
   fuel?: string;
 }
+
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+
+export interface showMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+}
